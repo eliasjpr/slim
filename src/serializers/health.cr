432e6b926f0608faa56cc2c38937e757b45a3131
Elias Perez <elias.perez@nytimes.com>
2019-11-10 07:46:11 -0500

    format code

--- src/serializers/health.cr
+++ src/serializers/health.cr
@@ -3,15 +3,15 @@ module Serializers
     include Onyx::HTTP::View
 
     json data: {
-      status:    "pass",
-      app_name:  "Slim",
-      version:   "0.1.0",
-      cpu_count: System.cpu_count,
-      hostname:  System.hostname,
+      status:      "pass",
+      app_name:    "Slim",
+      version:     "0.1.0",
+      cpu_count:   System.cpu_count,
+      hostname:    System.hostname,
       environment: ENV["CRYSTAL_ENV"],
-      host:      ENV["HOST"],
-      port:      ENV["PORT"],
-      time:      Time.local,
+      host:        ENV["HOST"],
+      port:        ENV["PORT"],
+      time:        Time.local,
     }
   end
 end
