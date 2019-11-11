b55da625205c440a2ca2485c77658aa95efc004d
Elias Perez <elias.perez@nytimes.com>
2019-11-11 18:24:30 -0500

    fixes ci specs

--- spec/endpoints/spec_helper.cr
+++ spec/endpoints/spec_helper.cr
@@ -6,3 +6,4 @@ require "../../src/app"
 VERSION_1 = HTTP::Headers{"Accept" => "application/vnd.slim.v1+json"}
 CLIENT    = HTTP::Client.new ENV["HOST"], ENV["PORT"].to_i
 
+Process.new("./bin/slim").wait
