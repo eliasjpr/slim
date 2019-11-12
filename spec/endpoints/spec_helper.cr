9e5d8735c4facaf9b92f2610a42714739edfcb8c
Elias Perez <elias.perez@nytimes.com>
2019-11-11 21:14:12 -0500

    Remove process

--- spec/endpoints/spec_helper.cr
+++ spec/endpoints/spec_helper.cr
@@ -5,5 +5,3 @@ require "../../src/app"
 
 VERSION_1 = HTTP::Headers{"Accept" => "application/vnd.slim.v1+json"}
 CLIENT    = HTTP::Client.new ENV["HOST"], ENV["PORT"].to_i
-
-Process.new(command: "./bin/slim", shell: true)
