8613c85f0e408147eb9d8bb078d5f21b8413ebf5
Elias Perez <elias.perez@nytimes.com>
2019-11-12 15:22:23 -0500

    Removes unecessary dependencies

--- spec/endpoints/spec_helper.cr
+++ spec/endpoints/spec_helper.cr
@@ -1,7 +1,5 @@
 require "spec"
 require "http"
-require "../helpers"
-require "../../src/app"
 
 VERSION_1 = HTTP::Headers{"Accept" => "application/vnd.slim.v1+json"}
 CLIENT    = HTTP::Client.new ENV["HOST"], ENV["PORT"].to_i
